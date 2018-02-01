97c2ce7bc3791d5932215cc8c87b1ffdc410cceb
José Manuel Rivas <rivasbornos@gmail.com>
2018-02-01 01:04:25 +0100

    Added new jsonfield to the model in example project for testing #PR5

--- example/characters/models.py
+++ example/characters/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Character(models.Model):
     name = models.CharField(max_length=200)
     data = JSONField()
+    other_data = JSONField()
 
     def __str__(self):  # __unicode__ on Python 2
         return self.name
