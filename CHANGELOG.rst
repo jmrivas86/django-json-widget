e88f81489e47e66b9d0d77579f3f8d700d7e10cc
jmrivas <jmrivas86@gmail.com>
2020-01-16 00:36:07 +0100

    Update django version, added more django verions to setup.py and added changelog

---
+++ CHANGELOG.rst
@@ -0,0 +1,15 @@
+=========
+Changelog
+=========
+
+
+0.3.0 (2020-01-16)
+==================
+
+* Update requirements.txt
+* Fixed static to work with CDN tools like django-storages
+* Make widget more configurable
+* Fixed an incompatibility with Python 2.7
+* update jsoneditor to latest version
+
+
--- setup.py
+++ setup.py
@@ -64,6 +64,9 @@ setup(
         'Development Status :: 3 - Alpha',
         'Framework :: Django',
         'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
@@ -73,5 +76,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
