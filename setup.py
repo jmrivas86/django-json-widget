9aed61c0b34b4c7273ac8573e905fff2beaa09f0
Erfan Arefmehr <work.arefmehr@gmail.com>
2025-08-20 11:08:57 +0330

    feat: add ruff settings as linter and formatter

--- setup.py
+++ setup.py
@@ -70,12 +70,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
 )
